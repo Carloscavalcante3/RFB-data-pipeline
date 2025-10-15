f6658e3da1bef727f3121bc5b18c00934b33ee78
Carloscavalcante3 <carlos.cvlnt@gmail.com>
2025-10-15 15:13:19 -0300

    feat: Adicionar arquivo de configurações

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# --- Configuração do Usuário ---
+# Pasta raiz onde as subpastas de ano e mês serão criadas.
+PASTA_RAIZ_DESTINO = r" "
+
+# Dados de Conexão com o PostgreSQL
+# Adicione suas Informações aqui
+CONFIG_BD = {
+    "usuario": " ", 
+    "senha": " ",       
+    "host": " ",    
+    "porta": " ",            
+    "banco": " "   
+}
+
+# Pastas de trabalho temporárias.
+PASTA_DOWNLOADS = "dados_originais_zip"
+PASTA_EXTRAIDOS_TEMP = "dados_extraidos_temp"
